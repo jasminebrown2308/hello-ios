796c99b8ed879f78fcca015cd731b1868114e60e
jasminebrown2308 <jasminebrown2308@gmail.com>
2021-06-07 20:56:16 +0100

    Initial Commit

---
+++ Hello/HelloApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloApp.swift
+//  Hello
+//
+//  Created by Jasmine Brown on 07/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct HelloApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
